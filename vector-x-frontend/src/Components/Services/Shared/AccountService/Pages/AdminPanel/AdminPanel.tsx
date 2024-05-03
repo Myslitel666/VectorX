bee392ec0909c8c321036e41431a60ae044ade96
Myslitel666 <tretyakov_02@bk.ru>
2024-05-03 22:58:09 +0500

    Added a filter bar to sort users in the admin panel.

--- vector-x-frontend/src/Components/Services/Shared/AccountService/Pages/AdminPanel/AdminPanel.tsx
+++ vector-x-frontend/src/Components/Services/Shared/AccountService/Pages/AdminPanel/AdminPanel.tsx
@@ -7,11 +7,18 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useTheme } from '@mui/material';
 
 //MyComponents Import
+import Header from '../../../../../Common/Header/Header';
 import { useUserContext } from '../../../../../../Context/UserContext';
+import { useColorLabel } from '../../../../../../Context/UseColorLabel';
+import AdminPanelIcon from './AdminPanelIcon';
+import MyButton from '../../../../../Common/User Interface/MyButton';
 
 const AdminPanel: React.FC = () => {
+    //Элементы для навигации
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -19,8 +26,15 @@ const AdminPanel: React.FC = () => {
     const { getUser } = useUserContext();
     const user = getUser();
 
+    //Работа с контекстом цветовой темы
+    const { getColorFromLabel } = useColorLabel('green');
+    const theme = useTheme();
+    const AdminPanelIconColor = theme.palette.background.default;
+    //const borderBoxColor = theme.palette.action.disabled;
+
     // Состояние для хранения id пользователя
     const [userIds, setUserId] = useState('');
+    const [username, setUsername] = useState('');
     const wsUrl = process.env.REACT_APP_WS_URL as string;
 
     const connectWebSocket = () => {
@@ -45,25 +59,90 @@ const AdminPanel: React.FC = () => {
 
     return (
         <>
+            <Header />
             <Box 
                 sx = {{
                     margin: 'auto',
-                    marginTop: '1rem',
+                    marginTop: '6rem',
                     alignItems: 'center',
-                    width: '50%',
+                    width: '63%',
                 }}
             >
-                <Typography variant="h5"
+                <Box 
+                    display='flex'
                     sx = {{
-                        margin: 'auto',
                         marginTop: '1rem',
                         marginBottom: '1rem',
-                        textAlign: 'center'
+                        justifyContent: 'center',
+                        alignItems: 'center'
                     }}
                 >
-                    Admin Panel
-                </Typography>
-                <Box display='flex'>
+                    <Box
+                        display="flex"
+                        alignItems="center"
+                        justifyContent="center"
+                        width="4.75rem"
+                        height="4.75rem"
+                        borderRadius="50%"
+                        bgcolor="primary.main"
+                        sx={{ transition: 'background-color 1s ease',
+                            marginRight: '0.66rem'
+                         }}
+                    >
+                        <AdminPanelIcon
+                        style = {{
+                            marginTop: '-0.35rem',
+                            fill: AdminPanelIconColor,
+                            height: '3.5rem',
+                            width: '3.5rem',
+                        }}
+                    />
+                    </Box>
+                    <Typography 
+                        sx = {{
+                            fontSize: '2.5rem'
+                        }}
+                    >
+                        Admin Panel
+                    </Typography>
+                </Box>
+                <Box 
+                    display='flex'
+                    sx = {{
+                        marginTop: '2rem'
+                    }}
+                >
+                    <TextField
+                        size ='small'
+                        label="Username"
+                        variant="outlined"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        fullWidth
+                        sx = {{
+                            marginLeft: '1rem',
+                            marginRight: '1rem'
+                        }}
+                    />
+                    <Button 
+                        //variant="contained" 
+                        sx = {{
+                            fontSize: '2rem',
+                            width: '8.5rem',
+                            height: '2.5rem',
+                            marginRight: '1rem',
+                            transition: 'color 1s ease'
+                        }}
+                    >
+                        Filter
+                    </Button>
+                </Box>
+                <Box 
+                    display='none' // В любой момент можно отобразить строку для быстрого ввода id'шников в целях тестирования Web Socket Manager'а
+                    sx = {{
+                        marginTop: '2rem'
+                    }}
+                >
                     <TextField
                         label="User IDs"
                         variant="outlined"
@@ -75,7 +154,7 @@ const AdminPanel: React.FC = () => {
                             marginRight: '1rem'
                         }}
                     />
-                    <Button 
+                    <MyButton 
                         variant="contained" 
                         onClick={handleButtonClick}
                         sx = {{
@@ -85,7 +164,7 @@ const AdminPanel: React.FC = () => {
                         }}
                     >
                         Send User IDs
-                    </Button>
+                    </MyButton>
                 </Box>
             </Box>
         </>
