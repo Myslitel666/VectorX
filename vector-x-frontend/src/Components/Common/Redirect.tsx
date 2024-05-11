71b46978fb5a3c650b7df1bb9f5dfff8465fcdcf
Myslitel666 <tretyakov_02@bk.ru>
2024-05-11 17:32:26 +0500

    The home page is declared the default page.

--- vector-x-frontend/src/Components/Common/Redirect.tsx
+++ vector-x-frontend/src/Components/Common/Redirect.tsx
@@ -2,21 +2,11 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-//MyComponents Import
-import { useUserContext } from '../../Context/UserContext';
-
 export default function Redirect({ link = '/home' }) {
     const navigate = useNavigate();
-    const { getUser } = useUserContext();
-    const user = getUser()
 
     useEffect(() => {
-        if (user.userId !== -1) {
-            navigate(link);
-        }
-        else {
-            navigate('/auth');
-        }
+        navigate(link);
     });
 
     return (
