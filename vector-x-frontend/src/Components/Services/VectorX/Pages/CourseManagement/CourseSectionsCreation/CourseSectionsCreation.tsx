71a6b606b240391dbfb582249e084622847f1c57
Myslitel666 <tretyakov_02@bk.ru>
2024-06-18 14:39:19 +0500

    Added a check for the number of sections in the course when deleting a section.

--- vector-x-frontend/src/Components/Services/VectorX/Pages/CourseManagement/CourseSectionsCreation/CourseSectionsCreation.tsx
+++ vector-x-frontend/src/Components/Services/VectorX/Pages/CourseManagement/CourseSectionsCreation/CourseSectionsCreation.tsx
@@ -213,11 +213,13 @@ const CourseSectionsCreation: React.FC = () => {
                                             minWidth: isDesktop ? '3.25rem' : '2.66rem',
                                             padding: isDesktop ? '0.75rem' : '0.5rem',
                                         }}
-                                        onClick = {() => {
-                                            deleteCourseSection(section.courseSectionId)        
-                                                .then(() => {
-                                                    fetchЫSections();
-                                                })
+                                        onClick={() => {
+                                            if (sections.length > 1) {
+                                                deleteCourseSection(section.courseSectionId)
+                                                    .then(() => {
+                                                        fetchЫSections();
+                                                    })
+                                                }
                                             }
                                         }
                                     >
